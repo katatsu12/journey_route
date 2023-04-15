048100ffdacce2a0fdfdabbe927e55fe2af82610
Den Zubritskiy <katatsu12@gmail.com>
2023-04-15 22:57:26 +0300

    Add current sign in at column to users table and add current_sign_in_at to schema.rb.

--- db/migrate/20230415185447_devise_create_users.rb
+++ db/migrate/20230415185447_devise_create_users.rb
@@ -3,12 +3,13 @@
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :email,              null: false, default: ''
-      t.string :encrypted_password, null: false, default: ''
+      t.string   :email,              null: false, default: ''
+      t.string   :encrypted_password, null: false, default: ''
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
       t.datetime :remember_created_at
       t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
       t.string   :current_sign_in_ip
       t.string   :last_sign_in_ip
--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_15_185447) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
