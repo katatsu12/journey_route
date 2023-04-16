cf1564bbe53f396a82b212f4c552461c14a94dd5
Den Zubritskiy <katatsu12@gmail.com>
2023-04-16 22:10:51 +0300

    Update application styling and views, and add Tailwind config custom variables.

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,9 +9,24 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      textColor: {
+        dark: {
+          base: 'var(--color-text-base)',
+          muted: 'var(--color-text-muted)',
+          inverted: 'var(--color-text-inverted)'
+        }
       },
+      backgroundColor: {
+        dark: {
+          fill: 'var(--color-fill)',
+          'button-accent': 'var(--color-button-accent)',
+          'button-accent-hover': 'var(--color-button-accent-hover)',
+          'button-muted': 'var(--color-button-muted)'
+        }
+      }
+      // fontFamily: {
+      //   sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      // },
     },
   },
   plugins: [
